7d51497366816fbaca30910555d88bb745a13b3c
iaw47941842 <iaw47941842@h17.informatica.escoladeltreball.org>
2017-10-04 17:02:16 +0200

    Main class added

---
+++ src/org/escoladeltreball/collectionsdemo1/Main.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package org.escoladeltreball.collectionsdemo1;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * @author iaw47941842
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> list = new ArrayList();
+		
+		list.add("Alpha");
+		list.add("Charlie");
+		list.add("Bravo");
+		list.add("Echo");
+		list.add("Delta");
+		list.add("Zulu");
+		
+		list.remove("Charlie");
+		
+		Collections.sort(list);
+		
+		for (String string : list) {
+			System.out.println(string);			
+		}
+		
+		
+		
+		
+		
+	}
+
+}
